9b64c5f099d185262f7cf8f51b9b6a3473858ea0
Keinix <zackcosborn@gmail.com>
2018-05-06 13:41:37 +0900

    finished FoundationPile and added relevent references to the GameModel

---
+++ src/GameModel.kt
@@ -0,0 +1,6 @@
+class GameModel {
+    val deck = Deck()
+    val wastePile: MutableList<Card> = mutableListOf()
+    val foundationPiles = arrayOf(FoundationPile(clubs), FoundationPile(diamonds),
+            FoundationPile(hearts), FoundationPile(spades))
+}
---
+++ src/TableauPile.kt
@@ -0,0 +1,5 @@
+/**
+ * Created by Keinix on 5/6/18.
+ */
+class TableauPile(var cards: MutableList<Card>) {
+}
