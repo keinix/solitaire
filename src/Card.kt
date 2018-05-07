c218404ad2209b54abd3e2f5259a430ea295b513
Keinix <zackcosborn@gmail.com>
2018-05-07 12:09:23 +0900

    finished with TableauPile (before tests)

--- src/Card.kt
+++ src/Card.kt
@@ -3,6 +3,9 @@ val diamonds = "Diamonds"
 val hearts = "Hearts"
 val spades = "Spades"
 
+val redSuit = arrayOf(diamonds, hearts)
+val blackSuit = arrayOf(clubs, spades)
+
 class Card(val value: Int, val suit: String, var faceUp: Boolean = false)
 
 
--- src/TableauPile.kt
+++ src/TableauPile.kt
@@ -1,5 +1,36 @@
-/**
- * Created by Keinix on 5/6/18.
- */
-class TableauPile(var cards: MutableList<Card>) {
+
+class TableauPile(var cards: MutableList<Card> = mutableListOf()) {
+
+    init {
+        if (cards.size > 0) {
+            cards.last().faceUp = true
+        }
+    }
+
+    fun addCards(newCards: MutableList<Card>): Boolean {
+        if (cards.size > 0) {
+            if (newCards.first().value == cards.last().value - 1 &&
+                    suitCheck(newCards.first(), cards.last())) {
+                cards.addAll(newCards)
+                return true
+            }
+        } else if (newCards.first().value == 12) {
+            cards.addAll(newCards)
+            return true
+        }
+         return false
+    }
+
+    fun removeCards(tappedIndex: Int) {
+        for (i in tappedIndex..cards.lastIndex) {
+            cards.removeAt(i)
+        }
+        if (cards.size > 0) {
+            cards.last().faceUp = true
+        }
+    }
+
+    private fun suitCheck(card1: Card, card2: Card) =
+        !(blackSuit.contains(card1.suit) && blackSuit.contains(card2.suit) ||
+                !(redSuit.contains(card1.suit) && redSuit.contains(card2.suit)))
 }
