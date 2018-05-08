31593091471a37329d9e8f52441e3d18194d3e29
Keinix <zackcosborn@gmail.com>
2018-05-08 19:27:28 +0900

    started work on gameplay methods

--- src/GameModel.kt
+++ src/GameModel.kt
@@ -4,4 +4,49 @@ class GameModel {
     val foundationPiles = arrayOf(FoundationPile(clubs), FoundationPile(diamonds),
             FoundationPile(hearts), FoundationPile(spades))
     val tableauPiles = Array(7, { TableauPile() })
+
+    fun reset() {
+        wastePile.clear()
+        foundationPiles.forEach { it.reset() }
+        deck.reset()
+        
+        tableauPiles.forEachIndexed { i, tableauPile ->
+            val cardsInPile: MutableList<Card> = Array(i +1, { deck.drawCard() }).toMutableList()
+            tableauPiles[i] = TableauPile(cardsInPile)
+        }
+    }
+
+   fun onDeckTap() {
+       if (deck.cardsInDeck.size > 0) {
+           val card = deck.drawCard()
+           card.faceUp = true
+           wastePile.add(card)
+       } else {
+           deck.cardsInDeck = wastePile.toMutableList()
+           wastePile.clear()
+       }
+   }
+
+    fun onWasteTap() {
+        if (wastePile.size > 0) {
+            val card = wastePile.last()
+            if (playCard(card)) {
+                wastePile.remove(card)
+            }
+        }
+    }
+
+    private fun playCard(card: Card): Boolean {
+        foundationPiles.forEach {
+            if (it.addCard(card)){
+                return true
+            }
+        }
+        tableauPiles.forEach {
+            if  (it.addCards(mutableListOf(card))) {
+                return true
+            }
+        }
+        return false
+    }
 }
