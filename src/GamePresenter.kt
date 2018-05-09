71726241b0cf3916f27ab5a80322db3258f49807
Keinix <zackcosborn@gmail.com>
2018-05-09 18:54:38 +0900

    game logic finished

---
+++ src/GamePresenter.kt
@@ -0,0 +1,25 @@
+class GamePresenter {
+    var view: GameView? = null
+
+    fun onDeckTap() = GameModel.onDeckTap()
+
+    fun setGameView(gameView: GameView) {
+        view = gameView
+        view?.update()
+    }
+
+    fun onWasteTap() {
+        GameModel.onWasteTap()
+        view?.update()
+    }
+
+    fun onFoundtationTab(foundationIndex: Int) {
+        GameModel.onFoundtationTab(foundationIndex)
+        view?.update()
+    }
+
+    fun onTableauTaped(tableauIndex: Int, cardIndex: Int) {
+        GameModel.onTableauTaped(tableauIndex, cardIndex)
+        view?.update()
+    }
+}
